a1761d8e8d65bb92dd8f6e28eb94471fbca393be
Alex <exe102343@gmail.com>
2015-05-09 17:49:26 +0800

    hw4 almost finished

--- hw4/forest.cpp
+++ hw4/forest.cpp
@@ -9,6 +9,7 @@
 #include <utility>
 #include <algorithm>
 #include <math.h>
+#include <time.h>
 using namespace std;
 class Example
 {
@@ -110,25 +111,19 @@ void printtab(int tabs)
 	for(int i = 0 ; i < tabs ; i ++)
 		printf("\t");
 }
-void buildtree( double eplison , vector<int> &cases ,int totaltrue , int totalfalse , int existfeature[] , int tabs)
+void buildtree( vector<int> &cases ,int totaltrue , int totalfalse , int existfeature[] , int tabs)
 {
 	int casesize = cases.size();
 	if(totaltrue == 0 && totalfalse != 0)
 	{
 		printtab(tabs);
-		printf("return -1;\n"  );
+		printf("F++;\n"  );
 		return;
 	}
 	if(totaltrue != 0 && totalfalse == 0)
 	{
 		printtab(tabs);
-		printf("return 1;\n"  );
-		return;
-	}
-	if(confusion(totaltrue , totalfalse) <= eplison)
-	{
-		printtab(tabs);
-		printf("return %d;\n" ,( (totaltrue > totalfalse) ? 1 : -1) );
+		printf("T++;\n"  );
 		return;
 	}
 	Best best(-1 , -1 , -1 , make_pair(0 , 0) , make_pair(0 , 0));
@@ -142,7 +137,7 @@ void buildtree( double eplison , vector<int> &cases ,int totaltrue , int totalfa
 	if(best.feapos == -1)
 	{
 		printtab(tabs);
-		printf("return %d;\n", ((totaltrue > totalfalse) ? 1 : -1) );
+		printf("%c++;\n", ((totaltrue > totalfalse) ? 'T' : 'F') );
 		return;
 	}
 	vector<int> newcase1 , newcase2;
@@ -155,23 +150,48 @@ void buildtree( double eplison , vector<int> &cases ,int totaltrue , int totalfa
 	}
 	printtab(tabs);
 	printf("if(attr[%d] > %lf){\n" ,best.feapos + 1, best.threshold);
-	buildtree(eplison , newcase1 , best.rightdecision.first , best.rightdecision.second , existfeature , tabs+1);
+	buildtree(newcase1 , best.rightdecision.first , best.rightdecision.second , existfeature , tabs+1);
 	printtab(tabs);
 	printf("}\n");
 	printtab(tabs);
 	printf("else{\n");
-	buildtree(eplison , newcase2 , best.leftdecision.first , best.leftdecision.second , existfeature , tabs+1);
+	buildtree(newcase2 , best.leftdecision.first , best.leftdecision.second , existfeature , tabs+1);
 	printtab(tabs);
 	printf("}\n");
 }
+void knuthshuffle(vector<int> &cases)
+{
+	int size = cases.size();
+	for(int i = size - 1 ; i >= 1 ; i--)
+	{
+		int randnum = rand() % i + 1;
+		swap(cases[i] , cases[randnum]);
+	}
+}
 
+void buildforest(int treenumber , vector<int> &cases , int totaltrue , int totalfalse , int existfeature[] , int tabs)
+{
+	int size = cases.size();
+	printf("int T = 0 , F = 0;\n");
+	for(int i = 0 ; i < treenumber ; i++)
+	{
+		printf("tree%d_predict:\n" , i + 1);
+		knuthshuffle(cases);
+		vector<int> randcase(cases.begin() , cases.begin() + (size / 3) );
+		buildtree(randcase , totaltrue , totalfalse , existfeature , tabs );
+	}
+	printf("voting:\n");
+	printf("\tif(T > F) return 1;\n");
+	printf("\telse return -1;\n");
+}
 int main(int argc , char *argv[])
 {
 	ifstream fin;
 	fin.open(argv[1]);
-	double eplison = atof(argv[2]);
+	int treenumber = atof(argv[2]);
 	string expression;
 	vector<int> cases;
+	srand(time(NULL));
 	int totaltrue = 0  , totalfalse = 0 , count = 0 , existfeature[1500] = {0};
 	int tabs = 1;
 	while(getline(fin , expression))
@@ -180,8 +200,7 @@ int main(int argc , char *argv[])
 		cases.push_back(count);
 		count ++;
 	}
-	printf("int tree_predict(double *attr){\n");
-	buildtree(eplison , cases  , totaltrue , totalfalse , existfeature , tabs);
+	printf("int forest_predict(double *attr){\n");
+	buildforest(treenumber , cases  , totaltrue , totalfalse , existfeature , tabs);
 	printf("}\n");
-	//cout << confusion(totaltrue , totalfalse);
 }
